f0c02c54f729b5a5dbf9196aec9590ac859eb30b
Paul Estrada <pestrada@tiempodevelopment.com>
2017-03-15 17:39:22 -0600

    adding test for login page

--- test/integration/index.js
+++ test/integration/index.js
@@ -1,10 +1,23 @@
-var assert = require('assert');
+var assert = require('chai').assert;
 var request = require('request');
 
 describe('index', function () {
-    it('should render index page', function() {
-        request('http://localhost:3000/index', function (error, response, body) {
-            assert(response.statusCode, 200);
+    it('renders index page', function (done) {
+        request.get('http://localhost:3000/', function (error, response, body) {
+            assert(response.statusCode == 200);
+            done();
         });
     });
+});
+
+describe('login', function () {
+    it('renders login form', function (done) {
+        request.get('http://localhost:3000/login', function (error, response, body) {
+            assert(response.statusCode == 200, 'error on request response');
+            assert(body.includes('Login page'), 'page does not include text');
+            done();
+        });
+    });
+
+    
 });
