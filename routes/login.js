24f4b4c7a32a9830911e18ea8219b4e445bba023
Paul Estrada <pestrada@tiempodevelopment.com>
2017-03-16 12:43:47 -0600

    mock functionality for login

--- routes/login.js
+++ routes/login.js
@@ -2,7 +2,20 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('login', { title: 'Login page' });
+  res.render('login', { title: 'Login page', username: 'paul' });
+});
+
+router.post('/', function(req, res, next) {
+    var user = {
+        name: 'paul',
+        password: '123'
+    };
+    
+    if (req.body.username == user.name && req.body.password == user.password) {
+        res.render('index', {username: user.name});
+    } else {
+        res.send('username or password is incorrect.');
+    }
 });
 
 module.exports = router;
--- test/integration/index.js
+++ test/integration/index.js
@@ -14,10 +14,23 @@ describe('login', function () {
     it('renders login form', function (done) {
         request.get('http://localhost:3000/login', function (error, response, body) {
             assert(response.statusCode == 200, 'error on request response');
-            assert(body.includes('Login page'), 'page does not include text');
+            assert(body.includes('Login page') == true, 'page does not include text');
             done();
         });
     });
 
-    
+    it('performs user authentication', function (done) {
+        var params = {
+            url: 'http://localhost:3000/login',
+            form: {
+                username: 'paul',
+                password: '123'
+            }
+        };
+        request.post(params, function (error, response, body) {
+            assert(response.statusCode == 200, 'error on request response');
+            assert(body.includes('Hello paul') == true, 'user was not logged in');
+            done();
+        });
+    });
 });
