267aa0fefaf3f1a76fe4da10de1f4afe683bb3e6
Paul Estrada <pestrada@tiempodevelopment.com>
2017-03-22 13:35:12 -0600

    configure material design lite

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Badge, Icon } from 'react-mdl';
 
 class App extends Component {
   render() {
@@ -13,6 +14,10 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+
+        <Badge text="1" overlap>
+          <Icon name="account_box" />
+        </Badge>
       </div>
     );
   }
