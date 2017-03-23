082e1ded3388e700f8bc2824f9e19c47404b04ef
Paul Estrada <pestrada@tiempodevelopment.com>
2017-03-23 17:45:45 -0600

    styling buttons

--- client/src/ListItem.js
+++ client/src/ListItem.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Card, CardTitle, CardText, CardActions, CardMenu, IconButton, Badge } from 'react-mdl';
+import { Card, CardTitle, CardText, CardActions, CardMenu, IconButton, Badge, Button } from 'react-mdl';
 
 class App extends Component {
   constructor(props) {
@@ -22,7 +22,11 @@ class App extends Component {
           {this.props.description}
         </CardText>
         <CardActions border>
-          <IconButton colored ripple name="add" />
+          <div>
+            <IconButton colored ripple name="add" className="action-button" />
+            <Button raised colored className="action-button">Approve</Button>
+            <Button raised colored className="action-button">Reject</Button>
+          </div>
         </CardActions>
         <CardMenu className="white">
             <Badge text="4">votes</Badge>
