7fa202f97ba00412075d975054fa0b51157c1980
Paul Estrada <pestrada@tiempodevelopment.com>
2017-03-28 16:06:50 -0600

    clear input value after closing dialog

--- client/src/RequestDialog.js
+++ client/src/RequestDialog.js
@@ -10,11 +10,17 @@ class RequestDialog extends React.Component {
   }
 
   onChange = (e) => {
-      this.setState({description: e.target.value});
+    this.setState({description: e.target.value});
   }
 
   handleClick = () => {
-      this.props.saveRequest(this.state.description);
+    this.props.saveRequest(this.state.description);
+    this.setState({description: ''});
+  }
+
+  handleCloseDialog = () => {
+    this.props.handleCloseDialog();
+    this.setState({description: ''});
   }
 
   render() {
@@ -23,16 +29,16 @@ class RequestDialog extends React.Component {
         <Dialog open={this.props.openDialog}>
           <DialogTitle>What would you like to request?</DialogTitle>
           <DialogContent>
-            <Textfield onChange={this.onChange} label="Text..." floatingLabel style={{width: '200px'}} />
+            <Textfield value={this.state.description} onChange={this.onChange} label="description..." floatingLabel style={{width: '200px'}} />
             <p>Chances of approval are higher if requests are voted up by your peers!</p>
           </DialogContent>
           <DialogActions>
             <Button type='button' onClick={this.handleClick}>Save</Button>
-            <Button type='button' onClick={this.props.handleCloseDialog}>Cancel</Button>
+            <Button type='button' onClick={this.handleCloseDialog}>Cancel</Button>
           </DialogActions>
         </Dialog>
       </div>
-    );
+      );
   }
 
 }
