5c448af8defc521ecc8c9f30169ed334bffb68f7
Paul Estrada <pestrada@tiempodevelopment.com>
2017-03-28 13:58:49 -0600

    using mongoose to connect MongoDB

--- server/routes/requests.js
+++ server/routes/requests.js
@@ -1,5 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/test');
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error'));
+db.once('open', function () {
+  console.log('*** Connected to MongoDB !! ***');
+});
 
 var requests = [
   {
