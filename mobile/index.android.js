4288773ac5505e25b40f3293a56f0809cc63ae49
Paul Estrada <pestrada@tiempodevelopment.com>
2017-04-06 17:59:37 -0500

    make native-base work

--- mobile/index.android.js
+++ mobile/index.android.js
@@ -11,22 +11,34 @@ import {
   Text,
   View
 } from 'react-native';
+import { Container, Header, Title, Content, Footer, FooterTab, Button, Left, Right, Body, Icon } from 'native-base';
 
 export default class mobile extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
+      <Container>
+        <Header>
+            <Left>
+                <Button transparent>
+                    <Icon name='menu' />
+                </Button>
+            </Left>
+            <Body>
+                <Title>Header</Title>
+            </Body>
+            <Right />
+        </Header>
+        <Content>
+            <Text>Riding React Native</Text>
+        </Content>
+        <Footer>
+            <FooterTab>
+                <Button full>
+                    <Text>Footer</Text>
+                </Button>
+            </FooterTab>
+        </Footer>
+      </Container>
     );
   }
 }
